53b124373444c6c1b914895c960a2076b8002a92
limc <mingcich@126.com>
2020-09-04 22:42:54 +0800

    init

---
+++ AMSR2L1RPre.py
@@ -0,0 +1,38 @@
+import h5py
+from osgeo import gdal
+
+
+class AMSR2L1RPre(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.hr_lon = None
+        self.lr_lon = None
+        self.hr_lat = None
+        self.lr_lat = None
+
+
+        pass
+
+    def reproj_roi(self):
+
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,10 @@
+
+import AMSR2L1RPre
+
+
+if __name__ == '__main__':
+
+
+
+
+    pass
